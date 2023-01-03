a4d4131a21f7fed647ca254ecb3d96a172010d94
zhangwule <97408479+zhangwule@users.noreply.github.com>
2023-01-03 22:40:36 +0800

    Add files via upload

---
+++ page/menus.js
@@ -0,0 +1,59 @@
+import * as api from "../../网页练习/电商网站/JavaScript/module.js";
+// console.log( api );
+
+// 侧边栏菜单
+{
+    let sidebarSwitch = document.querySelector( '#head-nav .sidebar-switch' );
+    let maskLayer = document.querySelector( '#head-nav .mask-layer' );
+    let sidebar  = document.querySelector( '#head-nav .menu-content' );
+
+    function unfoldSidebar() {
+        maskLayer.style.opacity = '1';
+        maskLayer.style.pointerEvents = 'auto';
+        sidebar.style.transform = 'translateX( 0px )';
+        page.style.overflowY = 'hidden';
+    }
+    function shutSidebar() {
+        maskLayer.style.opacity = '0';
+        maskLayer.style.pointerEvents = 'none';
+        sidebar.style.transform = 'translateX( 300px )';
+        page.style.overflowY = 'auto';
+    }
+    sidebarSwitch.addEventListener( 'click', unfoldSidebar );
+    maskLayer.addEventListener( 'click', shutSidebar );
+}
+
+
+
+{
+    // 装饰条
+    let decoration = document.querySelector( '#page .menus .decoration' );
+    let star = createStar();
+    // star = [ ...star ];
+    let timer = null;
+
+    function createStar() {
+        for ( let i = 0; i < Math.floor( decoration.clientWidth / 24 ) + 2; i++ ) {
+            let star = document.createElement( 'i' );
+            star.classList.add( 'icon-star', 'iconfont', 'star' );
+            decoration.appendChild( star );
+        }
+        return document.querySelectorAll( '#page .menus .decoration .star' );
+    }
+
+    let prevIndex = 0;
+    function shake() {
+        let index = api.random( 1, star.length - 2 );
+        star.forEach( item => {
+            item.classList.remove( 'shakeStar' );
+        } );
+        if ( prevIndex === index ) {
+            index = api.random( 1, star.length - 2 );
+        }
+        star[index].classList.add( 'shakeStar' );
+        prevIndex = index;
+    }
+    timer = setInterval( () => {
+        shake();
+    }, 3000 );
+}
---
+++ page/site-info.js
@@ -0,0 +1,41 @@
+// 文字输入效果
+{
+    let siteSubtitle = document.querySelector( '#site-info .site-subtitle .active-text' );
+
+    function textInputEffect() {
+        let textArray = [ 
+            '烟花只管绽放，何顾观者感受', 
+            '良言一句三冬暖，恶语伤人六月寒', 
+            '别离喧闹太近，别离自我太远' 
+        ];
+        let textCount = 0;
+        let sentencesCount = 0;
+        let timer = null; 
+        let isEnd = false;
+
+        function inputEffect() {
+            isEnd = false;
+            textCount++;
+            siteSubtitle.innerHTML = textArray[sentencesCount].substring( 0, textCount );
+            if ( siteSubtitle.innerHTML === textArray[sentencesCount] ){
+                sentencesCount++;
+                textCount = 0;
+                isEnd = true;
+            }
+            if ( sentencesCount === textArray.length ){
+                sentencesCount = 0;
+            }
+        }
+        timer = setInterval( () => {
+            if ( isEnd !== true ){ 
+                inputEffect();
+            }
+            else {
+                setTimeout( () => {
+                    isEnd = false;
+                }, 1600 );
+            }
+        }, 400 );
+    }
+    textInputEffect();
+}
