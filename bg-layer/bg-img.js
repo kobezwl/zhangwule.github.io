7b1d3aee9b942a68756df87fe64bb6646f7de8fe
zhangwule <97408479+zhangwule@users.noreply.github.com>
2023-01-04 21:59:52 +0800

    Add files via upload

--- bg-layer/bg-img.js
+++ bg-layer/bg-img.js
@@ -1,15 +1,18 @@
 // 背景图片随时间变化
 {
-    let bgImg = document.querySelector( '#bg-img' );
     let thisTimeHour = new Date().toTimeString().slice( 0, 2 ).split( ':' ).join( '' );
+    let bgImg = document.querySelector( '#bg-img' );
     let siteInfo = document.querySelector( '#site-info' );
+    let footer = document.querySelector( '#page-footer' );
 
-    if ( thisTimeHour > 12 ) {
-        bgImg.style.backgroundImage = `url( './image/background/城市夕阳_1.png' )`;
-        siteInfo.style.color = '#ffffff';
+    if ( thisTimeHour < 12 ) {
+        bgImg.style.backgroundImage = `url( './image/background/城市白天.png' )`;
+        siteInfo.style.color = '#000000';
+        bgImg.style.backgroundImage = `url( './image/background/城市白天.png' )`;
     }
     else {
-        bgImg.style.backgroundImage = `url( './image/background/城市白天_1.png' )`;
-        siteInfo.style.color = '#000000';
+        bgImg.style.backgroundImage = `url( './image/background/城市夕阳.png' )`;
+        siteInfo.style.color = '#ffffff';
+        bgImg.style.backgroundImage = `url( './image/background/城市夕阳.png' )`;
     }
 }
