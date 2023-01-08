1da465db56783717e670e470b2aaa4d462fd0cd4
zhangwule <97408479+zhangwule@users.noreply.github.com>
2023-01-08 21:10:46 +0800

    Add files via upload

--- index page/init/init.js
+++ index page/init/init.js
@@ -1,5 +1,16 @@
 // 'use strict';
 
+// 评论数
 {
-    // 
+    let postComment = document.querySelectorAll( '#posts .post-comment' );
+    let count = 0;
+    if ( localStorage.getItem( 'comment' ) ) {
+        count = localStorage.getItem( 'comment' ).split( ',' ).length;
+    }
+    else {
+        count = 0;
+    }
+    postComment.forEach( item => {
+        item.innerHTML = count;
+    } );
 }
